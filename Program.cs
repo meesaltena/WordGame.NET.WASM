943c7ac3f9926ac966e2f342d1f825938771d4c9
meesaltena <Mees.altena@gmail.com>
2024-04-27 18:06:23 +0200

    animations

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Wordle.NET.WASM;
 using System.Reflection;
+using Wordle.NET.WASM;
 [assembly: AssemblyVersion("1.0.*")]
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
