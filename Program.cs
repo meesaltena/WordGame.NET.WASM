333048adf30aa612758976407a921babc6b2daf8
meesaltena <Mees.altena@gmail.com>
2024-04-28 13:29:41 +0200

    win game modal styling

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Reflection;
+using Wordle.NET.Models;
 using Wordle.NET.WASM;
 [assembly: AssemblyVersion("1.0.*")]
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<NotifierService>();
+builder.Services.AddSingleton<TimerService>();
 
 await builder.Build().RunAsync();
---
+++ TimerService.cs
@@ -0,0 +1,62 @@
+﻿namespace Wordle.NET.Models
+{
+    public class TimerService(NotifierService notifier,
+        ILogger<TimerService> logger) : IDisposable
+    {
+        private int elapsedCount;
+        private readonly static TimeSpan heartbeatTickRate = TimeSpan.FromSeconds(5);
+        private readonly ILogger<TimerService> logger = logger;
+        private readonly NotifierService notifier = notifier;
+        private PeriodicTimer? timer;
+        public void Stop()
+        {
+            if (timer != null)
+            {
+                timer.Dispose();
+                timer = null;
+                logger.LogInformation("Stopped");
+            }
+        }
+
+        public async Task Start(TimeSpan? tickInterval, bool oneTime = true)
+        {
+            if (timer is null)
+            {
+                timer = new(tickInterval ?? heartbeatTickRate);
+                logger.LogInformation("Started");
+
+                using (timer)
+                {
+                    while (await timer.WaitForNextTickAsync())
+                    {
+                        elapsedCount += 1;
+                        await notifier.Update("elapsedCount", elapsedCount);
+                        logger.LogInformation("ElapsedCount {Count}", elapsedCount);
+                    }
+                    logger.LogInformation("sjalsdjads");
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+
+            // The following prevents derived types that introduce a
+            // finalizer from needing to re-implement IDisposable.
+            GC.SuppressFinalize(this);
+        }
+    }
+    public class NotifierService
+    {
+        public async Task Update(string key, int value)
+        {
+            if (Notify != null)
+            {
+                await Notify.Invoke(key, value);
+            }
+        }
+
+        public event Func<string, int, Task>? Notify;
+    }
+}
