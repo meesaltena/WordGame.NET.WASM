ccbd6436d2b2875c511f21aafa0f833bc5be2775
meesaltena <Mees.altena@gmail.com>
2024-05-02 15:27:51 +0200

    add allowed guess list

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Reflection;
 using Wordle.NET.Models;
 using Wordle.NET.WASM;
+using Wordle.NET.WASM.Utility;
 [assembly: AssemblyVersion("1.0.*")]
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<NotifierService>();
 builder.Services.AddSingleton<TimerService>();
+builder.Services.AddTransient<FileUtils>();
 
 await builder.Build().RunAsync();
---
+++ Utility/FileUtils.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+
+namespace Wordle.NET.WASM.Utility
+{
+    public class FileUtils
+    {
+        public List<string> GetWordsFromAssembly(string filename)
+        {
+            List<string> lines = new();
+            string wordsResource = $"Wordle.NET.WASM.Words.{filename}";
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                {
+                    using Stream stream = assembly.GetManifestResourceStream(wordsResource)!;
+                    if (stream is not null)
+                    {
+                        using StreamReader streamReader = new(stream);
+                        while (!streamReader.EndOfStream)
+                        {
+                            string line = streamReader.ReadLine()!;
+                            lines.Add(line.ToUpperInvariant());
+                        }
+                    } else
+                    {
+                        Console.WriteLine($"Error: Missing \"{wordsResource}\" embedded resource.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return lines;
+        }
+    }
+}
